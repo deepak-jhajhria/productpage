b60c912b9bd8914e36b71fbc5507a0de43ed73a7
Deepakjhajhria <deepakjhajhria4657@gmail.com>
2023-10-05 11:01:47 +0530

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,47 @@
+let mobileView = document.querySelector(".mobileView");
+let menu = document.querySelector(".menu");
+let body = document.querySelector("body");
+let span1 = document.querySelector(".span1")
+let span2 = document.querySelector(".span2")
+let span3 = document.querySelector(".span3")
+let navbtn = document.querySelector(".nav-btn")
+
+menu.addEventListener("click", function () {
+    mobileView.classList.toggle("show");
+    body.classList.toggle("overflow-hidden");
+    span1.classList.toggle("span4");
+    span2.classList.toggle("span5");
+    span3.classList.toggle("span6");
+})
+navbtn.addEventListener("click", function () {
+    mobileView.classList.remove("show");
+    body.classList.remove("overflow-hidden");
+    span1.classList.remove("span4");
+    span2.classList.remove("span5");
+    span3.classList.remove("span6");
+})
+ 
+let count = 1
+let show = document.getElementById("show")
+let increase = document.getElementById("increase")
+let decrease = document.getElementById("decrease")
+let addbutton = document. getElementById("addtocart")
+
+
+increase.addEventListener("click", function(){
+    count++;
+    show.innerHTML = count;
+});
+decrease.addEventListener("click", function(){
+    if(count > 1){count--;
+    show.innerHTML = count;}
+    
+});
+addbutton.addEventListener("click", function(){
+     alert(count +  " Items added to your cart");
+     
+});
+
 function openTab(tabId) {
 
     var tabButtons = document.querySelectorAll(".tab-button");
